e19e5c694a4636487fe8b61eb1f667e87cb268aa
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2022-01-19 22:08:57 +0200

    trees.in_order implementation

--- trees.v
+++ trees.v
@@ -1,6 +1,8 @@
 Set Warnings "-notation-overridden,-parsing".
 From Coq Require Import Arith.Arith.
 From LF Require Import Lists.
+From LF Require Import Poly.
+Import Lists.
 
 Inductive tree X : Type :=
 | empty : tree X
@@ -8,3 +10,20 @@ Inductive tree X : Type :=
 
 Arguments empty {X}.
 Arguments node {X} _ _ _.
+
+Fixpoint in_order {X : Type} (t : tree X) : list X :=
+  match t with
+  | empty => []
+  | node l x r => (in_order l) ++ [x] ++ (in_order r)
+  end
+.
+
+Example in_order_example_1 :
+  in_order (node
+    (node (node empty 1 empty) 3 (node empty 2 empty))
+    0
+    (node (node empty 4 empty) 1 (node empty 5 empty))
+  )
+  =
+  [1; 3; 2; 0; 4; 1; 5]
+. Proof. reflexivity. Qed.
