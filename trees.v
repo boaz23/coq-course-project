922ee965f170329914f2c9a72fff4b45cc121de9
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2022-01-19 22:10:28 +0200

    trees - example tree

--- trees.v
+++ trees.v
@@ -18,12 +18,14 @@ Fixpoint in_order {X : Type} (t : tree X) : list X :=
   end
 .
 
-Example in_order_example_1 :
-  in_order (node
+Definition example_tree : tree nat := (
+  node
     (node (node empty 1 empty) 3 (node empty 2 empty))
     0
     (node (node empty 4 empty) 1 (node empty 5 empty))
   )
-  =
-  [1; 3; 2; 0; 4; 1; 5]
-. Proof. reflexivity. Qed.
+.
+
+Example in_order_example_1 :
+  in_order example_tree = [1; 3; 2; 0; 4; 1; 5].
+Proof. reflexivity. Qed.
